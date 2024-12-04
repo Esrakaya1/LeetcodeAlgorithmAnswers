6d00dc430332f7802232d2d3da0fd1c08b542dda
Esrakaya1 <kyesra7@gmail.com>
2024-12-04 14:26:21 +0300

    explanation is added

--- 1-TwoSum.cs
+++ 1-TwoSum.cs
@@ -1,5 +1,6 @@
 public class Solution
 {
+    // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
     public int[] TwoSum(int[] nums, int target)
     {
         for (int i = 0; i < nums.Length; i++)
