c5bc6f08da2ff7ede796e9bb63f3e75749d161d8
Esrakaya1 <kyesra7@gmail.com>
2024-12-07 20:38:42 +0300

    comment line is added

--- 4- LongestCommonPrefix.cs
+++ 4- LongestCommonPrefix.cs
@@ -2,14 +2,14 @@ public class Solution
 {
     public string LongestCommonPrefix(string[] strs)
     {
-        if (strs.Length == 1) return strs[0];
+        if (strs.Length == 1) return strs[0];  // It's gonna be just one prefix so we return it.
 
-        string prefix = "";
-        int index = 0;
+        string prefix = "";  // store the common prefix
+        int index = 0; // index of each character
 
         while (index != strs[0].Length)
         {
-            char compare = strs[0][index];
+            char compare = strs[0][index]; // compare is the standard character from the first string that all others must match
             for (int i = 1; i < strs.Length; i++)
             {
                 if (index == strs[i].Length || strs[i][index] != compare)
