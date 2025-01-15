9ddbf678c468f94664d3cd4a2b9e375456b789d7
Esrakaya1 <kyesra7@gmail.com>
2025-01-15 15:37:52 +0300

    answer is added

---
+++ 18 - BinaryTreeInOrderTraversal.cs
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        List<int> result = new List<int>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while (root != null || stack.Count > 0)
+        {
+            while (root != null)
+            {
+                stack.Push(root);
+                root = root.left;
+            }
+            root = stack.Pop();
+            result.Add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
+}
+
+// time complexity: O(n) n is the number of nodes in the tree
+// space complexity: O(n) as the algorithm uses a stack to store nodes.
