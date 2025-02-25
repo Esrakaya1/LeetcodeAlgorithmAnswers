8b4ae79a3b33fdbb7dd7c5883ba42e51c3db6e30
Esrakaya1 <kyesra7@gmail.com>
2025-02-25 12:24:30 +0300

    added comment line

--- 32 - BinaryTreePostOrderTraversal.cs
+++ 32 - BinaryTreePostOrderTraversal.cs
@@ -1,14 +1,17 @@
-public class Solution {
-    public IList<int> PostorderTraversal(TreeNode root) {
+public class Solution
+{
+    public IList<int> PostorderTraversal(TreeNode root)
+    {
         List<int> result = new List<int>();
-        if(root == null) return result;
+        if (root == null) return result;
 
         Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.Push(root);
 
-        while(stack.Count > 0) {
+        while (stack.Count > 0)
+        {
             TreeNode node = stack.Pop();
-            result.Insert(0,node.val);
+            result.Insert(0, node.val);
             if (node.left != null) stack.Push(node.left);
             if (node.right != null) stack.Push(node.right);
         }
@@ -16,5 +19,5 @@ public class Solution {
     }
 }
 
-// time complexity: O(n ^ 2), In the worst case, inserting at index 0 takes O(N) time per insertion. Since we do this for each node, the total cost becomes O(N²).
+// time complexity: O(N ^ 2), In the worst case, inserting at index 0 takes O(N) time per insertion. Since we do this for each node, the total cost becomes O(N²).
 // space complexity: O(n) due to the stack. In the worst case, the tree is linear and the height is O(n). In the best case, the tree is balanced and the height is O(logn).
